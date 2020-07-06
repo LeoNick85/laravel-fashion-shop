f8b25f9374fc18e9fb14b6ec6148cc2a11eefa8c
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-07-06 15:21:49 +0200

    Controller + Model

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cloth;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $all_clothes = Cloth::all();
+
+        dd($all_clothes);
+
+        return view("home","all_clothes");
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel Fashion Shop</title>
+    </head>
+    <body>
+        <h1>Prova</h1>
+    </body>
+</html>
